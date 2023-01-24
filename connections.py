1b3b630e5336224a3ae7e15d7f21cd3c81ba523f
Manuel Ledezma <92046228+manuelledezma687@users.noreply.github.com>
2023-01-23 21:26:09 -0300

    Update connections.py

--- connections.py
+++ connections.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-## DATABASE_URL = "mysql+mysqlconnector://root:password@127.0.0.1:3306/fivestars"
+DATABASE_URL = "mysql+mysqlconnector://root:password@127.0.0.1:3306/fivestars"
 
-
-engine = create_engine("postgresql://postgres_user:bF5WpzHgiJN9BBFUHKr7CzN1odv7Iw8A@dpg-cf788t94reb2e0bqrdtg-a:5432/postgres_fivestars")
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
